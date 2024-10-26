8043c4489c344938e6d6774a52dc8e0336116077
ArcaneCipher <desjardins.jarp@gmail.com>
2024-10-25 21:04:42 -0400

    updated RemoteInterface.js for Stretch: Broadcasting Message to Clients

--- src/RemoteInterface.js
+++ src/RemoteInterface.js
@@ -24,7 +24,7 @@ class RemoteInterface {
       // Important: This error handler  is different than the one below! - KV
       socket.on('error', (err) => {
         // ignore errors! - Without this callback, we can get a ECONNRESET error that crashes the server - KV
-      })
+      });
     })
       .on('connection', this.handleNewClient.bind(this))
       .on('error', (err) => {
@@ -33,47 +33,85 @@ class RemoteInterface {
         // throw err
       })
       .listen(PORT, () => {
-        console.log('opened server on', this.server.address())
-      })
+        console.log('opened server on', this.server.address());
+      });
   }
 
   idleBoot(client) {
     try {
-      client.write('you ded cuz you idled\n', () => client.end())
+      client.write('you ded cuz you idled\n', () => client.end());
     } catch (e) {
       // nothing to do really.
     }
   }
 
   resetIdleTimer(client, time) {
-    if (client.idleTimer) clearTimeout(client.idleTimer)
+    if (client.idleTimer) clearTimeout(client.idleTimer);
     client.idleTimer = setTimeout(
       this.idleBoot.bind(this, client),
       time
-    )
+    );
+  }
+
+  // Broadcast a message to all connected clients
+  broadcast(message) {
+    this.clients.forEach((client) => {
+      try {
+        client.write(message);
+      } catch (err) {
+        console.log("Failed to send message to client:", err);
+      }
+    });
+  }
+
+  // handle player count message
+  getPlayerCountMessage() {
+    const playerCount = this.clients.length;
+    if (playerCount === 1) {
+      return "1 player is connected.";
+    } else {
+      return `${playerCount} players are connected.`;
+    }
   }
 
   handleNewClient(client) {
-    // process.stdout.write('\x07')
-    client.setEncoding('utf8')
-    this.clients.push(client)
-    this.resetIdleTimer(client, MAX_IDLE_TIMEOUT / 2)
+    // process.stdout.write('\x07');
+    client.setEncoding('utf8');
+    this.clients.push(client);
+    this.resetIdleTimer(client, MAX_IDLE_TIMEOUT / 2);
 
-    if (this.newClientHandler) this.newClientHandler(client)
+    // Send the broadcast message to all clients
+    this.broadcast("A new player joined!\n");
 
-    client.on('data', this.handleClientData.bind(this, client))
-    client.on('end', this.handleClientEnded.bind(this, client))
+    // broadcast the current number of active connections
+    this.broadcast(this.getPlayerCountMessage());
+
+    if (this.newClientHandler) this.newClientHandler(client);
+
+    client.on('data', this.handleClientData.bind(this, client));
+    client.on('end', this.handleClientEnded.bind(this, client));
   }
 
   handleClientData(client, data) {
     if (this.clientDataHandler) {
-      if (this.clientDataHandler(data, client)) this.resetIdleTimer(client, MAX_IDLE_TIMEOUT)
+      if (this.clientDataHandler(data, client)) {
+        this.resetIdleTimer(client, MAX_IDLE_TIMEOUT); 
+      }
     }
   }
 
   handleClientEnded(client) {
-    if (client.idleTimer) clearTimeout(client.idleTimer)
-    if (this.clientEndHandler) this.clientEndHandler(client)
+    if (client.idleTimer) clearTimeout(client.idleTimer);
+    if (this.clientEndHandler) this.clientEndHandler(client);
+
+    // Remove the client from the list of clients on disconnection
+    this.clients = this.clients.filter((c) => c !== client);
+
+    // Send the broadcast message to all clients
+    this.broadcast("A player has left!\n");
+    
+    // Log the current number of active connections
+    this.broadcast(this.getPlayerCountMessage());
   }
 
   bindHandlers(clientDataHandler, newClientHandler, clientEndHandler) {
